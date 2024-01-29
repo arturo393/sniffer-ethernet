b33adc75dd2038e53e92bfa6ae9c46da7c81fd8a
arturo33 <arturo@sigma-telecom.com>
2024-01-29 11:13:59 -0300

    se arregla conexión a tcp server

--- ethernet_test/ethernet_test.py
+++ ethernet_test/ethernet_test.py
@@ -1,24 +1,25 @@
 import socket
 import logging
+import sys
+
+SNIFFER_SERVER_IP_ADDR = '192.168.60.101'
+SNIFFER_SERVER_TCP_PORT = 3000
+
 
 logging.basicConfig(filename="ethernet_test.log", level=logging.DEBUG)
-
-# Crea un objeto socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-logging.debug('socket ready {!r}')
-# Conecta el socket a la dirección IP y el puerto del servidor
-server_address = ('192.168.60.101', 3000)
-sock.connect(server_address)
-
-# Envía los datos al servidor
-message = 'Hola, mundo!'
-sock.sendall(message.encode())
-
-# Espera la respuesta del servidor
-data = sock.recv(1024)
-logging.debug('Recibido {!r}'.format(data))
-
-# Cierra la conexión
-sock.close()
-
- 
+        
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((SNIFFER_SERVER_IP_ADDR, SNIFFER_SERVER_TCP_PORT))
+            #sock.settimeout(2)
+            while True:
+                data = sock.recv(1024)  # Receive 1024 bytes at a time
+                logging.debug('Recibido {!r}'.format(data))
+                data = bytearray(data)
+                logging.debug("Message retransmitted:")
+                sock.sendall(data)
+                    
+    except Exception as e:
+        logging.error(e)
+        sys.exit()
